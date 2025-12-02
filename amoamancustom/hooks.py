c74280354c6fa63d3b754b7cfb3de1db38699ccf
KONE Fousseni <fkone@amoaman.com>
2025-12-02 12:24:39 +0000

    theme disable

--- amoamancustom/hooks.py
+++ amoamancustom/hooks.py
@@ -31,8 +31,8 @@ expense_claim.get_total_reimbursed_amount = get_total_reimbursed_amount
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/amoamancustom/css/brand_theme_desk.css"
-app_include_js = "/assets/amoamancustom/js/brand_theme_web.js"
+#app_include_css = "/assets/amoamancustom/css/brand_theme_desk.css"
+#app_include_js = "/assets/amoamancustom/js/brand_theme_web.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/amoamancustom/css/amoamancustom.css"
